5a7628cca5b05efd301fc18953b75342aa8e6eed
Dede Rahmat <dederamat102@gmail.com>
2025-01-14 06:17:03 +0700

    pass say welcome thread test case

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -8,6 +8,11 @@ const routes = (handler) => [
     method: 'GET',
     path: '/threads/{threadId}',
     handler: handler.getThreadHandler,
+  },  
+  {
+    method: 'GET',
+    path: '/threads/welcome',
+    handler: handler.getWelcomeThreadHandler,
   },
 ];
 
